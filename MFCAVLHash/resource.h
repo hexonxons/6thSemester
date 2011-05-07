6672e3aaff1e3893ba7aa4bd99817575bb22b19b
Alexander <killgamesh666@gmail.com>
2011-05-07 12:44:23 +0400

    ''

--- MFCAVLHash/resource.h
+++ MFCAVLHash/resource.h
@@ -14,6 +14,7 @@
 #define IDC_BTNDEL                      1006
 #define IDC_EDIT1                       1007
 #define IDC_EDIT2                       1008
+#define IDC_BUTTON1                     1009
 
 // Next default values for new objects
 // 
@@ -21,7 +22,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
